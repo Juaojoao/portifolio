dc85036599a1689f33c5092c91ad75cce59e8bd3
Jaum <soloqf@gmail.com>
2024-06-14 17:49:25 -0300

    Feat: Ajustado layout quebrado.

--- src/components/projetos/projetos.tsx
+++ src/components/projetos/projetos.tsx
@@ -3,7 +3,7 @@ import { projectData } from "../../data/projects";
 
 export const Projetos = () => {
   return (
-    <div className="shadow-xl bg-gray-900 sm:h-lvh w-full p-4 flex justify-center flex-col items-center gap-6">
+    <div className="shadow-xl bg-gray-900 sm:h-full w-full p-4 flex justify-center flex-col items-center gap-6">
       <div className="flex items-center justify-center gap-2">
         <div className="w-11 h-1 bg-yellow-600"></div>
         <h1 className="text-yellow-600">PROJETOS</h1>
